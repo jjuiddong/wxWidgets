27c36bc71fe694fe1271d0ee30e6b5f02c1cffd1
jjuiddong <jjuiddong@hanmail.net>
2013-02-28 01:17:04 -0800

    simple wxApp On WinAPI sample

--- wxAppOnAPI/wxAppOnAPI.cpp
+++ wxAppOnAPI/wxAppOnAPI.cpp
@@ -11,26 +11,12 @@
 #endif
 #include "wx/evtloop.h"
 
-// Define a new application type
 class MyApp: public wxApp
 {
 public:
 	virtual bool OnInit();
-
-	// we need to override this as the default behaviour only works when we're
-	// running wxWidgets main loop, not MFC one
 	virtual void ExitMainLoop();
-
 	wxFrame *CreateFrame();
-};
-
-class MyCanvas: public wxScrolledWindow
-{
-public:
-	MyCanvas(wxWindow *parent, const wxPoint& pos, const wxSize& size);
-	void OnPaint(wxPaintEvent& event);
-	void OnMouseEvent(wxMouseEvent& event);
-	DECLARE_EVENT_TABLE()
 };
 
 class MyChild: public wxFrame
@@ -38,36 +24,18 @@ class MyChild: public wxFrame
 public:
 	MyChild(wxFrame *frame, const wxString& title, const wxPoint& pos, const wxSize& size, const long style);
 	virtual ~MyChild();
-
-	void OnQuit(wxCommandEvent& event);
-	void OnNew(wxCommandEvent& event);
-	void OnActivate(wxActivateEvent& event);
-
-	MyCanvas *canvas;
-
 	DECLARE_EVENT_TABLE()
 };
 
-// ID for the menu quit command
-#define HELLO_QUIT 1
-#define HELLO_NEW  2
-
 DECLARE_APP(MyApp)
-
-// notice use of IMPLEMENT_APP_NO_MAIN() instead of the usual IMPLEMENT_APP!
 IMPLEMENT_APP_NO_MAIN(MyApp)
 
 bool MyApp::OnInit()
 {
 	if ( !wxApp::OnInit() )
 		return false;
-
-	// as we're not inside wxWidgets main loop, the default logic doesn't work
-	// in our case and we need to do this explicitly
 	SetExitOnFrameDelete(true);
-
-	(void) CreateFrame();
-
+	CreateFrame();
 	return true;
 }
 
@@ -81,121 +49,23 @@ wxFrame *MyApp::CreateFrame()
 {
 	MyChild *subframe = new MyChild(NULL, wxT("Canvas Frame"), wxPoint(10, 10), wxSize(300, 300),
 		wxDEFAULT_FRAME_STYLE);
-
 	subframe->SetTitle(wxT("wxWidgets canvas frame"));
-
-	// Give it a status line
-	subframe->CreateStatusBar();
-
-	// Make a menubar
-	wxMenu *file_menu = new wxMenu;
-
-	file_menu->Append(HELLO_NEW, wxT("&New MFC Window"));
-	file_menu->Append(HELLO_QUIT, wxT("&Close"));
-
-	wxMenuBar *menu_bar = new wxMenuBar;
-
-	menu_bar->Append(file_menu, wxT("&File"));
-
-	// Associate the menu bar with the frame
-	subframe->SetMenuBar(menu_bar);
-
-	int width, height;
-	subframe->GetClientSize(&width, &height);
-
-	MyCanvas *canvas = new MyCanvas(subframe, wxPoint(0, 0), wxSize(width, height));
-//	canvas->SetCursor(wxCursor(wxCURSOR_PENCIL));
-	subframe->canvas = canvas;
-	subframe->Show(true);
-
-	// Return the main frame window
+ 	subframe->Show(true);
 	return subframe;
 }
 
-BEGIN_EVENT_TABLE(MyCanvas, wxScrolledWindow)
-EVT_PAINT(MyCanvas::OnPaint)
-EVT_MOUSE_EVENTS(MyCanvas::OnMouseEvent)
-END_EVENT_TABLE()
-
-// Define a constructor for my canvas
-MyCanvas::MyCanvas(wxWindow *parent, const wxPoint& pos, const wxSize& size)
-: wxScrolledWindow(parent, -1, pos, size)
-{
-}
-
-// Define the repainting behaviour
-void MyCanvas::OnPaint(wxPaintEvent& WXUNUSED(event))
-{
-	wxPaintDC dc(this);
-
-	dc.SetFont(* wxSWISS_FONT);
-	dc.SetPen(* wxGREEN_PEN);
-	dc.DrawLine(0, 0, 200, 200);
-	dc.DrawLine(200, 0, 0, 200);
-
-	dc.SetBrush(* wxCYAN_BRUSH);
-	dc.SetPen(* wxRED_PEN);
-	dc.DrawRectangle(100, 100, 100, 50);
-	dc.DrawRoundedRectangle(150, 150, 100, 50, 20);
-
-	dc.DrawEllipse(250, 250, 100, 50);
-	dc.DrawLine(50, 230, 200, 230);
-	dc.DrawText(wxT("This is a test string"), 50, 230);
-}
-
-// This implements a tiny doodling program! Drag the mouse using
-// the left button.
-void MyCanvas::OnMouseEvent(wxMouseEvent& event)
-{
-	static long s_xpos = -1;
-	static long s_ypos = -1;
-
-	wxClientDC dc(this);
-	dc.SetPen(* wxBLACK_PEN);
-	wxPoint pos = event.GetPosition();
-	if (s_xpos > -1 && s_ypos > -1 && event.Dragging())
-	{
-		dc.DrawLine(s_xpos, s_ypos, pos.x, pos.y);
-	}
-
-	s_xpos = pos.x;
-	s_ypos = pos.y;
-}
-
 BEGIN_EVENT_TABLE(MyChild, wxFrame)
-EVT_MENU(HELLO_QUIT, MyChild::OnQuit)
-EVT_MENU(HELLO_NEW, MyChild::OnNew)
-EVT_ACTIVATE(MyChild::OnActivate)
 END_EVENT_TABLE()
 
 MyChild::MyChild(wxFrame *frame, const wxString& title, const wxPoint& pos, const wxSize& size, const long style)
 : wxFrame(frame, -1, title, pos, size, style)
 {
-	canvas = NULL;
 }
 
 MyChild::~MyChild()
 {
 	if ( IsLastBeforeExit() )
 		PostQuitMessage(0);
-}
-
-void MyChild::OnQuit(wxCommandEvent& WXUNUSED(event))
-{
-	Close(true);
-}
-
-void MyChild::OnNew(wxCommandEvent& WXUNUSED(event))
-{
-// 	CMainWindow *mainWin = new CMainWindow();
-// 	mainWin->ShowWindow( TRUE );
-// 	mainWin->UpdateWindow();
-}
-
-void MyChild::OnActivate(wxActivateEvent& event)
-{
-	if (event.GetActive() && canvas)
-		canvas->SetFocus();
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -213,7 +83,6 @@ TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
@@ -257,21 +126,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	return (int) msg.wParam;
 }
 
-
-
-//
-//  함수: MyRegisterClass()
-//
-//  목적: 창 클래스를 등록합니다.
-//
-//  설명:
-//
-//    Windows 95에서 추가된 'RegisterClassEx' 함수보다 먼저
-//    해당 코드가 Win32 시스템과 호환되도록
-//    하려는 경우에만 이 함수를 사용합니다. 이 함수를 호출해야
-//    해당 응용 프로그램에 연결된
-//    '올바른 형식의' 작은 아이콘을 가져올 수 있습니다.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
@@ -293,25 +147,12 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassEx(&wcex);
 }
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   설명:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    HWND hWnd;
-
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-
    hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-
+      CW_USEDEFAULT, 0, 300, 300, NULL, NULL, hInstance, NULL);
    if (!hWnd)
    {
       return FALSE;
@@ -332,16 +173,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  목적: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND	- 응용 프로그램 메뉴를 처리합니다.
-//  WM_PAINT	- 주 창을 그립니다.
-//  WM_DESTROY	- 종료 메시지를 게시하고 반환합니다.
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
@@ -356,9 +187,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		// 메뉴의 선택 영역을 구문 분석합니다.
 		switch (wmId)
 		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
 		case IDM_EXIT:
 			DestroyWindow(hWnd);
 			break;
@@ -378,24 +206,4 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
-}
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
 }
